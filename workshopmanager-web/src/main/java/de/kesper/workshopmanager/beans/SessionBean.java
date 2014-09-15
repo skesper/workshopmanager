e3effd6d4937d44fd229846f527c064674212cf5
kesper <stephan.kesper@gmail.com>
2014-09-15 16:54:54 +0200

    intermediate

--- workshopmanager-web/src/main/java/de/kesper/workshopmanager/beans/SessionBean.java
+++ workshopmanager-web/src/main/java/de/kesper/workshopmanager/beans/SessionBean.java
@@ -16,9 +16,14 @@ import javax.inject.Named;
 @SessionScoped
 public class SessionBean implements Serializable {
 
+    private boolean authenticated = false;
+    
     private String version = "v1.0";
     
     private float valueAddedTax = 19.F;
+    
+    private String userName;
+    private String password;
 
     public float getValueAddedTax() {
         return valueAddedTax;
@@ -34,5 +39,34 @@ public class SessionBean implements Serializable {
 
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+    
+    public String doLogin() {
+        authenticated = true;
+        return "OK";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
---
+++ workshopmanager-web/src/main/java/de/kesper/workshopmanager/filter/LoginFilter.java
@@ -0,0 +1,53 @@
+/*
+ *  15.09.2014 (C) KV RLP
+ */
+package de.kesper.workshopmanager.filter;
+
+import de.kesper.workshopmanager.beans.SessionBean;
+import java.io.IOException;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kesper
+ */
+public class LoginFilter implements Filter {
+    
+    private static final Logger log = Logger.getLogger("LoginFilter");
+    
+    @Inject
+    private SessionBean sessionBean;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        
+        if (sessionBean.isAuthenticated()) {
+            chain.doFilter(request, response);
+            return;
+        }
+        log.info("Denied request for "+httpRequest.getSession().getId());
+        ((HttpServletResponse)response).sendRedirect("/workshopmanager-web/index.xhtml");
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}
